d2b9583ddde9dbbdf69324e682e3a08326656a50
hkopp12 <hkopp1@uwyo.edu>
2024-11-11 21:51:24 +0000

    changes

--- cosc1010-lab09.py
+++ cosc1010-lab09.py
@@ -7,10 +7,15 @@
 # Your
 # Comments
 # Here
+
+
+
 # Classes
 # For this assignment, you will be creating two classes:
 # One for Pizza
 # One for a Pizzeria
+
+
 # You will be creating a Pizza class. It should have the following attributes:
 # - Size
 # - Sauce
@@ -19,27 +24,32 @@
 # which returns the corresponding attribute, just the value.
 # For the size and toppings attributes, you will need to have a method to set them.
 # - For Size, ensure it is an int > 10 (inches)
-# - If it is not, default to a 10" pizza (you can store ten). These checks should
-occur in init as well.
+# - If it is not, default to a 10" pizza (you can store ten). These checks should occur in init as well.
 # - For toppings, you will need to add the toppings.
 # - This method needs to be able to handle multiple values.
 # - Append all elements to the list.
 # Create a method that returns the amount of toppings.
 # In your __init__() method, you should take in size and sauce as parameters.
 # - Sauce should have a default value of red.
-# - Size will not have a default value; use the parameter with the same safety
-checks defined above (you can use the set method).
+# - Size will not have a default value; use the parameter with the same safety checks defined above (you can use the set method).
 # Within __init__(), you will need to:
 # - Assign the parameter for size to a size attribute.
 # - Assign the parameter for sauce to the attribute.
 # - Create the toppings attribute, starting off as a list only holding cheese.
+
+class Pizza:
+    def __init__(self, size, sauce='red'):
+        self.size = self.set_size(size)
+        self.sauce = sauce
+        self.toppings = ['cheese']
+
+
+
 # You will be creating a Pizzeria class with the following attributes:
 # - orders, the number of orders placed. Should start at 0.
 # - price_per_topping, a static value for the price per topping of 0.30.
-# - price_per_inch, a static value of 0.60 to denote how much the pizza cost per
-inch of diameter.
-# - pizzas, a list of all the pizzas with the last ordered being the last in the
-list.
+# - price_per_inch, a static value of 0.60 to denote how much the pizza cost per inch of diameter.
+# - pizzas, a list of all the pizzas with the last ordered being the last in the list.
 # You will need the following methods:
 # - __init__()
 # - This one does not need to take in any extra parameters.
@@ -50,8 +60,7 @@ list.
 # - It will need to create a pizza object.
 # - You will need to prompt the user for:
 # - the size
-# - the sauce, tell the user if nothing is entered it will default to red sauce
-(check for an empty string).
+# - the sauce, tell the user if nothing is entered it will default to red sauce (check for an empty string).
 # - all the toppings the user wants, ending prompting on an empty string.
 # - Implementation of this is left to you; you can, for example:
 # - have a while loop and append new entries to a list
@@ -59,8 +68,7 @@ list.
 # - Upon completion, create the pizza object and store it in the list.
 # - getPrice()
 # - You will need to determine the price of the pizza.
-# - This will be (pizza.getSize() * price_per_inch) + pizza.getAmountOfToppings()
-* price_per_topping.
+# - This will be (pizza.getSize() * price_per_inch) + pizza.getAmountOfToppings() * price_per_topping.
 # - You will have to retrieve the pizza from the pizza list.
 # - getReceipt()
 # - Creates a receipt of the current pizza.
@@ -70,6 +78,15 @@ list.
 # - The total price.
 # - getNumberOfOrders()
 # - This will simply return the number of orders.
+
+class Pizzeria:
+    price_per_topping = 0.30
+    price_per_inch = 0.60
+
+    def __init__(self):
+        self.orders = 0
+        self.pizzas = []
+        
 # - Declare your pizzeria object.
 # - Enter a while loop to ask if the user wants to order a pizza.
 # - Exit on the word `exit`.
